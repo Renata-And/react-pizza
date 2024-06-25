cd5e14aa50cb83880600f4435d556e9982a78e95
renata-and <renata.androsova@gmail.com>
2024-06-25 18:55:25 +0700

    Rendering pizza block, categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,14 +1,36 @@
+import { useState } from 'react';
+
 function Categories() {
+  const [activIndex, setActivIndex] = useState(0);
+  const categories = [
+    'Все',
+    'Мясные',
+    'Вегетарианская',
+    'Гриль',
+    'Острые',
+    'Закрытые',
+  ];
+  const onClickCategory = (index) => {
+    setActivIndex(index);
+  };
+
   return (
     <div className='categories'>
-      <ul>
-        <li className='active'>Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
-      </ul>
+      <div>
+        {categories.map((value, index) => {
+          return (
+            <button
+              key={index}
+              onClick={() => {
+                onClickCategory(index);
+              }}
+              className={activIndex === index ? 'active' : ''}
+            >
+              {value}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,34 +1,50 @@
 import { useState } from 'react';
 
-function PizzaBlock() {
+function PizzaBlock({ title, price, types, sizes, imageUrl }) {
   const [pizzaCount, setPizzaCount] = useState(0);
-  const onClickHandler = () => {
+  const [typeIndex, setTypeIndex] = useState(0);
+  const [sizeIndex, setSizeIndex] = useState(0);
+
+  const pizzasTypes = ['тонкое', 'традиционное'];
+
+  const onClickAdd = () => {
     setPizzaCount(pizzaCount + 1);
   };
 
   return (
     <div className='pizza-block'>
-      <img
-        className='pizza-block__image'
-        src='https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg'
-        alt='Pizza'
-      />
-      <h4 className='pizza-block__title'>Чизбургер-пицца</h4>
+      <img className='pizza-block__image' src={imageUrl} alt='Pizza' />
+      <h4 className='pizza-block__title'>{title}</h4>
       <div className='pizza-block__selector'>
         <ul>
-          <li className='active'>тонкое</li>
-          <li>традиционное</li>
+          {types.map((type, index) => {
+            return (
+              <li
+                onClick={() => setTypeIndex(index)}
+                className={typeIndex === index ? 'active' : ''}
+              >
+                {pizzasTypes[type]}
+              </li>
+            );
+          })}
         </ul>
         <ul>
-          <li className='active'>26 см.</li>
-          <li>30 см.</li>
-          <li>40 см.</li>
+          {sizes.map((size, index) => {
+            return (
+              <li
+                onClick={() => setSizeIndex(index)}
+                className={sizeIndex === index ? 'active' : ''}
+              >
+                {size} см.
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className='pizza-block__bottom'>
-        <div className='pizza-block__price'>от 395 ₽</div>
+        <div className='pizza-block__price'>от {price} ₽</div>
         <button
-          onClick={onClickHandler}
+          onClick={onClickAdd}
           className='button button--outline button--add'
         >
           <svg
